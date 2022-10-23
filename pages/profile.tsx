20f074d5a85fcaf8dad3eb8bd00c4f26ae31bcf7
echomonk <erdaloylupinar@gmail.com>
2022-10-23 19:02:07 +0300

    refactored paths

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
 import type { NextPage } from "next";
-import { BaseLayout } from "../components";
+import { BaseLayout } from "../components/ui";
 
 import nfts from "../content/meta.json";
 import { NftMeta } from "../types/nft";
