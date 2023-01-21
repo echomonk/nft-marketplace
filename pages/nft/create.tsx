68e6f5de271b186928efceaf736207e22657124e
echomonk <erdaloylupinar@gmail.com>
2023-01-21 17:12:13 +0300

    Create NFT form  handlers

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -1,16 +1,54 @@
 /* eslint-disable @next/next/no-img-element */
 
 import type { NextPage } from "next";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { BaseLayout } from "@ui";
 import { Switch } from "@headlessui/react";
 import Link from "next/link";
-
-const ATTRIBUTES = ["health", "attack", "speed"];
+import { NftMeta } from "@_types/nft";
 
 const NftCreate: NextPage = () => {
   const [nftURI, setNftURI] = useState("");
   const [hasURI, setHasURI] = useState(false);
+  const [nftMeta, setNftMeta] = useState<NftMeta>({
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+      { trait_type: "attack", value: "0" },
+      { trait_type: "health", value: "0" },
+      { trait_type: "speed", value: "0" },
+    ],
+  });
+
+  // Set Meta Data
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setNftMeta({ ...nftMeta, [name]: value });
+  };
+
+  // Set Attribute Values
+  const handleAttributeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    // Find the attribute index
+    const attributeIdx = nftMeta.attributes.findIndex(
+      (attr) => attr.trait_type === name
+    );
+
+    // Set the value
+    nftMeta.attributes[attributeIdx].value = value;
+    setNftMeta({
+      ...nftMeta,
+      attributes: nftMeta.attributes,
+    });
+  };
+
+  const createNft = () => {
+    console.log(nftMeta);
+  };
+
   return (
     <BaseLayout>
       <div>
@@ -141,6 +179,8 @@ const NftCreate: NextPage = () => {
                       </label>
                       <div className="mt-1 flex rounded-md shadow-sm">
                         <input
+                          value={nftMeta.name}
+                          onChange={handleChange}
                           type="text"
                           name="name"
                           id="name"
@@ -158,12 +198,13 @@ const NftCreate: NextPage = () => {
                       </label>
                       <div className="mt-1">
                         <textarea
+                          value={nftMeta.description}
+                          onChange={handleChange}
                           id="description"
                           name="description"
                           rows={3}
                           className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
                           placeholder="Some nft description..."
-                          defaultValue={""}
                         />
                       </div>
                       <p className="mt-2 text-sm text-gray-500">
@@ -180,7 +221,7 @@ const NftCreate: NextPage = () => {
                     ) : (
                       <div>
                         <label className="block text-sm font-medium text-gray-700">
-                          Cover photo
+                          Image
                         </label>
                         <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                           <div className="space-y-1 text-center">
@@ -221,21 +262,23 @@ const NftCreate: NextPage = () => {
                       </div>
                     )}
                     <div className="grid grid-cols-6 gap-6">
-                      {ATTRIBUTES.map((attribute) => (
+                      {nftMeta.attributes.map((attribute) => (
                         <div
-                          key={attribute}
+                          key={attribute.trait_type}
                           className="col-span-6 sm:col-span-6 lg:col-span-2"
                         >
                           <label
-                            htmlFor={attribute}
+                            htmlFor={attribute.trait_type}
                             className="block text-sm font-medium text-gray-700"
                           >
-                            {attribute}
+                            {attribute.trait_type}
                           </label>
                           <input
+                            onChange={handleAttributeChange}
+                            value={attribute.value}
                             type="text"
-                            name={attribute}
-                            id={attribute}
+                            name={attribute.trait_type}
+                            id={attribute.trait_type}
                             className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                           />
                         </div>
@@ -247,10 +290,11 @@ const NftCreate: NextPage = () => {
                   </div>
                   <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button
+                      onClick={createNft}
                       type="button"
                       className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                      Save
+                      List
                     </button>
                   </div>
                 </div>
